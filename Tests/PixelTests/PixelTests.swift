9ba26050a43194100460200a87e674b7db063218
Khaled Chehabeddine <kch2002@icloud.com>
2024-05-16 12:50:15 +0300

    Fixing base commit

--- Tests/PixelTests/PixelTests.swift
+++ Tests/PixelTests/PixelTests.swift
@@ -1,7 +1,16 @@
+//
+//  PixelTests.swift
+//  Pixel
+//
+//  Created by Khaled Chehabeddine on 16/05/2024.
+//  Copyright © 2024 Pixel. All rights reserved.
+//
+
 import XCTest
 @testable import Pixel
 
 final class PixelTests: XCTestCase {
+
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
