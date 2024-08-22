6bb6235f034e719d11299ff4d401627645fe4c83
Khaled Chehabeddine <kch2002@icloud.com>
2024-08-22 22:59:00 +0300

    Clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -12,14 +12,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // MARK: Local Modules
-
-
         // MARK: Packages
-
-
-        // MARK: Plugins
-//        .swiftLintPackageDependency
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -29,71 +22,49 @@ let package = Package(
     ]
 )
 
-fileprivate extension Package.Dependency {
-
-//    static let swiftLintPackageDependency: Package.Dependency = package(url: "https://github.com/realm/SwiftLint.git",
-//                                                                        from: "0.55.0")
+private extension Package.Dependency {
 }
 
-fileprivate extension Product {
+private extension Product {
 
-    static let pixelProduct: Product = .library(name: .pixel,
-                                                targets: [.pixel])
+    static let pixelProduct: Product = .library(name: .pixel, targets: [.pixel])
 }
 
-fileprivate extension String {
+private extension String {
 
     // MARK: Module
     static let module: String = "Pixel"
-
-    // MARK: Local Modules
-
 
     // MARK: Submodules
     static let pixel: String = "Pixel"
 
     // MARK: Packages
-    
 
     // MARK: Plugins
-//    static let swiftLint: String = "SwiftLint"
-//    static let swiftLintPlugin: String = "SwiftLintBuildToolPlugin"
+    static let swiftLint: String = "SwiftLint"
+    static let swiftLintPlugin: String = "SwiftLintBuildToolPlugin"
 
     var testTarget: String { "\(self)Tests" }
 }
 
-fileprivate extension SupportedPlatform {
+private extension SupportedPlatform {
 
     static let iOS: SupportedPlatform = .iOS(.v16)
 }
 
-fileprivate extension Target {
+private extension Target {
 
-    static let pixelTarget: Target = target(
-        name: .pixel
-//        plugins: [.swiftLintPlugin]
-    )
-    static let pixelTestTarget: Target = testTarget(
-        name: .pixel.testTarget,
-        dependencies: [.pixelDependency]
-//        plugins: [.swiftLintPlugin]
-    )
+    static let pixelTarget: Target = target(name: .pixel)
+    static let pixelTestTarget: Target = testTarget(name: .pixel.testTarget, dependencies: [.pixelDependency])
 }
 
-fileprivate extension Target.Dependency {
-
-    // MARK: Local Modules
-
+private extension Target.Dependency {
 
     // MARK: Submodules
     static let pixelDependency: Target.Dependency = byName(name: .pixel)
 
     // MARK: Packages
-
 }
 
-fileprivate extension Target.PluginUsage {
-
-//    static let swiftLintPlugin: Target.PluginUsage = plugin(name: .swiftLintPlugin,
-//                                                            package: .swiftLint)
+private extension Target.PluginUsage {
 }
