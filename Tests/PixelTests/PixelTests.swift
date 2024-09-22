3a06c94cf7ecca74ae834c2ac7f49224c3ad5d22
Khaled Chehabeddine <kch2002@icloud.com>
2024-09-22 11:55:54 +0300

    Remove cache and fix test runs

--- Tests/PixelTests/PixelTests.swift
+++ Tests/PixelTests/PixelTests.swift
@@ -12,10 +12,5 @@ import XCTest
 final class PixelTests: XCTestCase {
 
     func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
 }
